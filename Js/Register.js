b126eb701cb3af6cecdc420d4878c9ed950f555e
rohantare03 <rohatare123@gmail.com>
2022-07-01 19:00:41 +0530

    [Rohan] Add : Creating a Employee Registration Form

---
+++ Js/Register.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+   
+    AddDateField();
+  });
+function AddDateField() {
+    var x = document.getElementById("salary");
+    for (i = 1; i <= 20; i++) {
+        var option = document.createElement("option");
+        option.text = (i*10000).toString();
+        option.value = i*10000;
+        x.add(option);
+    }
+    
+    var d = new Date();
+    var n = d.getFullYear();
+    var x = document.getElementById("start-date-year");
+    var i;
+    for(i = n; i>1990; i--){
+        var option = document.createElement("option");
+        option.text = i.toString();
+        option.value = i;
+        x.add(option);
+    }
+    var x = document.getElementById("start-date-month");
+    for(i=1; i<= 12; i++){
+        var option = document.createElement("option");
+        option.text = i.toString();
+        option.value = i;
+        x.add(option);
+    }
+ var x = document.getElementById("start-date-day")
+    for(i =1 ; i <= 31; i++)
+    {
+        var option = document.createElement("option");
+        option.text = i.toString();
+        option.value = i;
+        x.add(option);
+    }
+}
+
+
+ 
